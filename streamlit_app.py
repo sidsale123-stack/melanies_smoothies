98f56b61b18c2d3c981c6f156659982307a2635d
sidsale123-stack <sidsale123@gmail.com>
2026-05-12 12:42:52 +0530

    fixing requests position

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests  
 
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write("""Choose the fruits you want in your custom Smoothie!""")
@@ -22,7 +23,9 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width =True)
+    
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
@@ -32,8 +35,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,' + name_on_order + '!', icon="✅")
-        
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response)
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width =True)
